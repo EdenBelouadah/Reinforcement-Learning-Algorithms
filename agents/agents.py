b2de2e65fb562ea3731c758f192ad5b1c018d356
Eden BELOUADAH <32595803+EdenBelouadah@users.noreply.github.com>
2017-11-25 15:26:55 +0100

    Add files via upload

---
+++ agents/agents.py
@@ -0,0 +1,79 @@
+"""
+File to complete. Contains the agents
+VEvalTemporalDifferencing and VEvalMonteCarlo (TD0)
+"""
+import numpy as np
+import math
+
+
+class Policy(object):
+    """ Base class for policies. Do not modify
+    """
+
+    def __init__(self):
+        super(Policy, self).__init__()
+
+    def action(self, mdp, state, values):
+        pass
+
+
+class RandomPolicy(Policy):
+    def __init__(self):
+        super(RandomPolicy, self).__init__()
+
+    def action(self, *args, **kwargs):
+        actions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        return actions[np.random.choice(range(len(actions)))]
+
+
+class VEvalTemporalDifferencing(object):
+    def __init__(self, mdp, policy, *args, **kwargs):
+        super(VEvalTemporalDifferencing, self).__init__()
+        self.mdp = mdp
+        self.policy = policy()
+        self.values = np.zeros(mdp.size)  # Store state values in this variable
+        self.learning_rate = kwargs.get('learning_rate', 0.1)
+        self.discount = kwargs.get('discount', 0.6)
+
+    def update(self):
+        # TO IMPLEMENT
+        # Ingredients : discount, values, learning_rate, old position, new position, reward,...
+        #
+        values=np.zeros_like(self.values)
+        for s in range(len(values)):
+            for s_prime in range(len(self.values)):
+                if s_prime!=s:
+                    values[s]+=0.25*self.values[s_prime]
+            values[s]*=self.discount
+            values[s]+=self.values[s]
+        self.values=values
+    
+
+    def action(self):
+        self.last_position = self.mdp.position
+        self.last_action = self.policy.action(
+            self.mdp, self.last_position, self.values)
+        return self.last_action
+
+
+class VEvalMonteCarlo(object):
+    def __init__(self, mdp, policy, *args, **kwargs):
+        super(VEvalMonteCarlo, self).__init__()
+        self.mdp = mdp
+        self.policy = policy
+        self.values = np.zeros(mdp.size)  # Store state values in this variable
+        self.sum_values = np.zeros(mdp.size)
+        self.n_transitions = np.zeros(mdp.size)
+        self.discount = kwargs.get('discount', 0.6)
+
+    def update(self):
+        # TO IMPLEMENT
+        # Ingredients: Reward, history of positions, values, discount,...
+        pass
+    
+
+    def action(self):
+        self.last_position = self.mdp.position
+        self.last_action = self.policy.action(
+            self.mdp, self.last_position, self.values)
+        return self.last_action
